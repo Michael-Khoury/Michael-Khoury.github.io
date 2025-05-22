e9d6c0f0cf6db96acbe70b25df6e94c40003a69a
Michael-Khoury <mkhoury@mun.ca>
2025-05-22 12:14:29 -0230

    fixed background and made project and work experience the consistent

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import WorkExperience from './pages/WorkExperience';
@@ -9,21 +9,30 @@ import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 
+function AppContent() {
+  const location = useLocation();
+  const isAltBackground = location.pathname === '/contact' || location.pathname === '/resume';
+
+  return (
+    <div className={`App ${isAltBackground ? 'alt-background' : ''}`}>
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<WorkExperience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<ContactMe />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="App">
-         <ScrollToTop />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/work" element={<WorkExperience />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<ContactMe />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
