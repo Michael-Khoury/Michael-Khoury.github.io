e026f5fb6f3ea8dadd425bf7d112b6196eeecaa0
Michael-Khoury <mkhoury@mun.ca>
2025-05-26 11:36:13 -0230

    launched back end

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -15,7 +15,7 @@ function ContactMe() {
     setStatus('Sending...');
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://backend-9v05.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
